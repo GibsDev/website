21336483159d1957f6df084bbe903f04c8c9de5b
GibsDev <crgibson2@live.com>
2016-07-07 22:05:11 -0400

    added some content to sidebar

--- res/php/sidebar.php
+++ res/php/sidebar.php
@@ -1,6 +1,15 @@
 		<div id="sidebar">
 			<div id="sidebar-content">
-				<h1 id="gibsdev">GibsDev</h1>
+				<div id="sidebar-wrapper">
+					<h1 id="title">GibsDev</h1>
+					<img id="gibsdev" src="/res/images/gibsdev.svg" alt="gibsdev" />
+					<a class="sidebar-link" href="">Links</a>
+					<a class="sidebar-link" href="">To</a>
+					<a class="sidebar-link" href="">Stuff</a>
+					<div id="footer">
+						<p>FOOTER</p>
+					</div>
+				</div>
 			</div>
 			<div id="shadow"></div>
 		</div>
